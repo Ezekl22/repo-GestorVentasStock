a6ebb16e9656cf556f67c4b94c1cd2b5e26dbde2
ezekl <ezekl@eze>
2021-08-07 13:20:15 -0300

    [MERGE][hotfix/leonardo22eze-primerComit]

---
+++ ServiceLasHeras/src/productos/MainPrueba.java
@@ -0,0 +1,7 @@
+package productos;
+
+public class MainPrueba {
+	public static void main (String []args) {
+		System.out.println("Hola sebas");
+	}
+}
---
+++ ServiceLasHeras/src/productos/producto.java
@@ -0,0 +1,17 @@
+package productos;
+
+public class producto {
+	//Atributos de la clase
+	
+	String nombre;
+	String marca;
+	String descripcion;
+	Double precioVenta;
+	Double precioCompra;
+	Integer proveedor;
+	Integer stock;
+	
+	
+	
+	
+}
